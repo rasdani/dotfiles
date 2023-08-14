288f72b1eb9a63a5b49605bdd6162677f971399c
rasdani <noreply@github.com>
2023-08-14 13:09:00 +0200

    dont overwrite stuff

--- run_before_00-install-packages.sh
+++ run_before_00-install-packages.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+### install packages, if not present
 sudo apt update
 if ! command -v tmux &> /dev/null
 then
@@ -19,7 +20,6 @@ fi
 
 if ! command -v pip &> /dev/null
 then
-    # Install pip using apt
     sudo apt install python3-pip
     echo "INSTALLED PIP"
 fi
@@ -34,11 +34,22 @@ then
     echo "INSTALLED ZSH"
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "INSTALLED OH-MY-ZSH"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "INSTALLED ZSH-AUTOSUGGESTIONS"
+### zsh plugins
+if ! [ -d ~/.oh-my-zsh ]
+then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "INSTALLED OH-MY-ZSH"
+fi
+
+if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]
+then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "INSTALLED ZSH-AUTOSUGGESTIONS"
+fi
+
+### don't touch picky .zshrc
 echo source $HOME/.oh-my-zsh/custom/custom_zshrc.zsh >> $HOME/.zshrc
+
 chsh -s $(which zsh)
 echo "CHANGED DEFAULT SHELL TO ZSH"
 echo "SETUP DONE"
