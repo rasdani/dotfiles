ae8124b14f4d393d17e56db2e9d9a087ff317f6e
rasdani <noreply@github.com>
2023-08-14 13:03:02 +0200

    tuned install script

--- run_before_00-install-packages.sh
+++ run_before_00-install-packages.sh
@@ -1,25 +1,44 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install -y tmux
-echo "installed tmux"
-sudo apt install -y neovim
-sudo apt install -y fasd
-echo "installed neovim"
-
-if command -v pip &> /dev/null
+if ! command -v tmux &> /dev/null
 then
-    echo "pip is installed"
-else
-    echo "pip is not installed"
+    sudo apt install -y tmux
+    echo "INSTALLED TMUX"
+fi
+if ! command -v nvim &> /dev/null
+then
+    sudo apt install -y neovim
+    echo "INSTALLED NEOVIM"
+fi
+if ! command -v fasd &> /dev/null
+then
+    sudo apt install -y fasd
+    echo "INSTALLED FASD"
+fi
+
+if ! command -v pip &> /dev/null
+then
     # Install pip using apt
     sudo apt install python3-pip
+    echo "INSTALLED PIP"
 fi
-pip install tldr
-sudo apt install -y zsh
-echo "installed zsh"
+if ! command -v tldr &> /dev/null
+then
+    pip install tldr
+    echo "INSTALLED TLDR"
+fi
+if ! command -v zsh &> /dev/null
+then
+    sudo apt install -y zsh
+    echo "INSTALLED ZSH"
+fi
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "installed oh-my-zsh"
+echo "INSTALLED OH-MY-ZSH"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "installed zsh-autosuggestions"
+echo "INSTALLED ZSH-AUTOSUGGESTIONS"
 echo source $HOME/.oh-my-zsh/custom/custom_zshrc.zsh >> $HOME/.zshrc
+chsh -s $(which zsh)
+echo "CHANGED DEFAULT SHELL TO ZSH"
+echo "SETUP DONE"
